27b0182b39c88e38cb6bd79277fc5b3c008c1e2d
Wilhelm <helmsen@hotmail.de>
2016-07-18 00:14:31 +0200

    update

---
+++ ContainerGRPCAdapterClient/callMethods.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+HOST=$1
+PORT=$2
+
+printf "\n"
+echo "Wait 10s until adapter is initialized"
+sleep 10s
+
+printf "\ņ"
+echo "Adapter address: ${HOST}:${PORT}"
+
+printf "\n"
+echo "Call reverse() on Google Cloud Functions adapter"
+out =$(curl ${HOST}:${PORT}/graphql -XPOST -H "Content-Type:application/graphql" --data "{reverse(service:\"LeonsTestService\", input:{name:\"Leon\"}){requestId}}")
+requestId=TODO
+echo "  RequestId: $requestId"
+
+printf "\n"
+echo "Call reverseStatus() on Google Cloud Functions adapter"
+out=$(curl ${HOST}:${PORT}/graphql -XPOST -H "Content-Type:application/graphql" --data "{reverseStatus(input:{requestId:\"${requestId}\"}){status,result{name}}}")
+echo "  Result:"
+echo "  $out"
---
+++ ContainerGoogleCloudFunctionsAdapter/README.md
@@ -0,0 +1,4 @@
+# Calling Public Google Cloud Functions
+
+```docker build -t tssa4t2ccfc .```
+```docker run a4t2ccfc```
